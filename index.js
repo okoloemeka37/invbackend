c3e1ed1d6f312cf0411a37858378c995d91e18b3
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-07-17 23:25:19 +0100

    Refactor index.js to include routers and middleware

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import "dotenv/config";
-import express from "express";
+import express from 'express';
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import authRouter from './routes/authRouter.js'
+import fieldRouter from './routes/fieldRouter.js'
+import agentRouter from './routes/agentRouter.js'
+import recordsRouter from './routes/recordsRouter.js'
+import parameterRouter from './routes/parameterRouter.js'
+const port=process.env.PORT
+const app=express();
 
-const app = express();
-const port = process.env.PORT;
+//http://localhost:3000
 
-app.get("/", (req, res) => {
-    res.send("Backend is working!");
-});
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+     origin:"https://client-95np.onrender.com",
+     credentials:true
+}))
 
-app.listen(port, () => {
-    console.log(`Running on ${port}`);
-});
+
+app.use("/api/auth/",authRouter);
+app.use("/api/field/",fieldRouter);
+app.use("/api/agent/",agentRouter);
+app.use("/api/parameter",parameterRouter);
+app.use("/api/records",recordsRouter);
+
+
+app.listen(port,()=>{
+    console.log(`App Running On Port ${port}`)
+}) 
