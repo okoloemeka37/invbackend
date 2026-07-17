2392c6725556ebf02c7646f04ffc95e6031489fe
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-07-17 23:07:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
 import "dotenv/config";
-import express from 'express';
-import cors from 'cors'
-import cookieParser from "cookie-parser";
-import authRouter from './routes/authRouter.js'
-import fieldRouter from './routes/fieldRouter.js'
-import agentRouter from './routes/agentRouter.js'
-import recordsRouter from './routes/recordsRouter.js'
-import parameterRouter from './routes/parameterRouter.js'
-const port=process.env.PORT
-const app=express();
+import express from "express";
 
-//http://localhost:3000
+const app = express();
+const port = process.env.PORT;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-     origin:"https://client-95np.onrender.com",
-     credentials:true
-}))
+app.get("/", (req, res) => {
+    res.send("Backend is working!");
+});
 
-
-app.use("/api/auth/",authRouter);
-app.use("/api/field/",fieldRouter);
-app.use("/api/agent/",agentRouter);
-app.use("/api/parameter",parameterRouter);
-app.use("/api/records",recordsRouter);
-
-
-app.listen(port,()=>{
-    console.log(`App Running On Port ${port}`)
-}) 
+app.listen(port, () => {
+    console.log(`Running on ${port}`);
+});
