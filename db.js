299cafc84d26dc21c30759b77579356b178f62cb
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-07-17 23:25:42 +0100

    Refactor database connection configuration

--- db.js
+++ db.js
@@ -1,45 +1,13 @@
-import mysql from 'mysql2/promise';
-
-const DB_NAME ='b8gnxtlgiy61lcjlezzh'  //'INV'// ;
-
-/*   const baseConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-} 
-  */
- 
- const baseConfig = {
-  host: 'b8gnxtlgiy61lcjlezzh-mysql.services.clever-cloud.com',
-  user: 'u9qyighrpu9qxsms',
-  password: 'hsuiO1rHMtGZiUX89x7p',
-}; 
- 
-/**
- * Ensure database exists
- */
-
-
-async function ensureDatabase() {
-  const connection = await mysql.createConnection(baseConfig);
-
-  await connection.query(
-    `CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\``
-  );
-
-  await connection.end();
-}
-
-/**
- * Create pool AFTER DB exists
- */
-await ensureDatabase();
+import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  ...baseConfig,
-  database: DB_NAME,
+  host: "b8gnxtlgiy61lcjlezzh-mysql.services.clever-cloud.com",
+  user: "u9qyighrpu9qxsms",
+  password: "hsuiO1rHMtGZiUX89x7p",
+  port: 3306,
+  database: "b8gnxtlgiy61lcjlezzh",
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
 });
 
 export default db;
