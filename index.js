6252f8301a61d675cc9f5d358faf6dee505c715b
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-07-18 09:43:44 +0100

    Comment out middleware and route handlers
    Comment out middleware and route handlers for auth, field, agent, parameter, and records.

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 import "dotenv/config";
 import express from 'express';
 import cors from 'cors'
-import cookieParser from "cookie-parser";
-import authRouter from './routes/authRouter.js'
-import fieldRouter from './routes/fieldRouter.js'
-import agentRouter from './routes/agentRouter.js'
-import recordsRouter from './routes/recordsRouter.js'
-import parameterRouter from './routes/parameterRouter.js'
+
+
 const port=process.env.PORT
 const app=express();
 
 //http://localhost:3000
 
 app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+//app.use(cookieParser())
+
+/*app.use(cors({
      origin:"https://client-95np.onrender.com",
      credentials:true
-}))
+})) */
 
 
-app.use("/api/auth/",authRouter);
+/*app.use("/api/auth/",authRouter);
 app.use("/api/field/",fieldRouter);
 app.use("/api/agent/",agentRouter);
 app.use("/api/parameter",parameterRouter);
-app.use("/api/records",recordsRouter);
+app.use("/api/records",recordsRouter);*/
 
 app.get("/health", (req, res) => {
     res.json({
