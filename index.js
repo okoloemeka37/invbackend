ace3848ff4262deddca6436b4b37637a9a56424b
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-07-20 21:12:23 +0100

    Refactor index.js to improve structure and imports

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
-console.log("=== NEW VERSION LOADED ===");
-
-//import "dotenv/config";
+import "dotenv/config";
 import express from 'express';
-//import cors from 'cors'
-//const port=process.env.PORT
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import authRouter from './routes/authRouter.js'
+import fieldRouter from './routes/fieldRouter.js'
+import agentRouter from './routes/agentRouter.js'
+import recordsRouter from './routes/recordsRouter.js'
+import parameterRouter from './routes/parameterRouter.js'
+const port=process.env.PORT
 const app=express();
-const port=3000;
+
 //http://localhost:3000
 
-//app.use(express.json())
-
-app.get("/health", (req, res) => {
-    res.send("EMEKA HEALTH CHECK");
-});
-
-//app.use(cookieParser())
-
-/*app.use(cors({
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
      origin:"https://client-95np.onrender.com",
      credentials:true
-})) */
+}))
 
 
-/*app.use("/api/auth/",authRouter);
+app.use("/api/auth/",authRouter);
 app.use("/api/field/",fieldRouter);
 app.use("/api/agent/",agentRouter);
 app.use("/api/parameter",parameterRouter);
-app.use("/api/records",recordsRouter);*/
-//app.get("/", (req, res) => {
- //   res.send("ROOT WORKS");
-//});
+app.use("/api/records",recordsRouter);
+
 
 app.listen(port,()=>{
-  console.log(`App Running On Port 3000`)
+    console.log(`App Running On Port ${port}`)
+}) 
+
+
+import "dotenv/config";
+import express from 'express';
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import authRouter from './routes/authRouter.js'
+import fieldRouter from './routes/fieldRouter.js'
+import agentRouter from './routes/agentRouter.js'
+import recordsRouter from './routes/recordsRouter.js'
+import parameterRouter from './routes/parameterRouter.js'
+const port=process.env.PORT
+const app=express();
+
+//http://localhost:3000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+     origin:"https://client-95np.onrender.com",
+     credentials:true
+}))
+
+
+app.use("/api/auth/",authRouter);
+app.use("/api/field/",fieldRouter);
+app.use("/api/agent/",agentRouter);
+app.use("/api/parameter",parameterRouter);
+app.use("/api/records",recordsRouter);
+
+
+app.listen(port,()=>{
+    console.log(`App Running On Port ${port}`)
 }) 
